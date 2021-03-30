a5d4c9b9b9020e57e78432b96da36df8ba92183e
vix, hi <hi_vix@163.com>
2021-03-30 22:47:23 +0800

    list dispaly

---
+++ lib/model/post.dart
@@ -0,0 +1,46 @@
+class Post {
+
+  final String title;
+  final String author;
+  final String imageUlr;
+
+  const Post({
+    this.title,
+    this.author,
+    this.imageUlr,
+  });
+
+
+}
+
+final List<Post> posts = [
+  Post(
+    title:'测试一',
+    author:'vix hi',
+    imageUlr:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F2c.zol-img.com.cn%2Fproduct%2F124_500x2000%2F984%2FceU7xYD3umwA.jpg&refer=http%3A%2F%2F2c.zol-img.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619704527&t=8fb92ccdcf7e483aa32a5d3d60ce4797',
+  ),
+
+    Post(
+    title:'测试二',
+    author:'vix2 ，hi',
+    imageUlr:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F2c.zol-img.com.cn%2Fproduct%2F124_500x2000%2F984%2FceU7xYD3umwA.jpg&refer=http%3A%2F%2F2c.zol-img.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619704527&t=8fb92ccdcf7e483aa32a5d3d60ce4797',
+  ),
+
+     Post(
+    title:'测试三',
+    author:'vix3 ，hi',
+    imageUlr:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F2c.zol-img.com.cn%2Fproduct%2F124_500x2000%2F984%2FceU7xYD3umwA.jpg&refer=http%3A%2F%2F2c.zol-img.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619704527&t=8fb92ccdcf7e483aa32a5d3d60ce4797',
+  ),
+
+     Post(
+    title:'测试四',
+    author:'vix4 ，hi',
+    imageUlr:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F2c.zol-img.com.cn%2Fproduct%2F124_500x2000%2F984%2FceU7xYD3umwA.jpg&refer=http%3A%2F%2F2c.zol-img.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619704527&t=8fb92ccdcf7e483aa32a5d3d60ce4797',
+  ),
+
+     Post(
+    title:'测试5',
+    author:'vix5 ，hi',
+    imageUlr:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F2c.zol-img.com.cn%2Fproduct%2F124_500x2000%2F984%2FceU7xYD3umwA.jpg&refer=http%3A%2F%2F2c.zol-img.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619704527&t=8fb92ccdcf7e483aa32a5d3d60ce4797',
+  ),
+];
