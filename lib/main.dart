1142569c43346312c987d5f8dba5274af5660b89
vix, hi <hi_vix@163.com>
2021-03-30 20:16:39 +0800

    main.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Hello world'),
+      home: MyHomePage(title: 'Hello flutter'),
     );
   }
 }
